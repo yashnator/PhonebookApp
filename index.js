a76e6fe9f33359e4d2b7ac64c94eaeca7166a414
Yash Solanki <252yash@gmail.com>
2022-10-05 21:29:20 +0530

    uibuild

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/api/persons/:id", (request, response) => {
   })
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if(body.name === undefined) {
     return response.status(400).json({error:'Content missing'})
@@ -41,15 +41,12 @@ app.post("/api/persons", (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+    .catch(error => next(error))
 });
 
 app.put('/api/persons/:id', (request,response,next) => {
-  const body = request.body
-  const person = {
-    name : body.name,
-    number : body.number
-  }
-  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  const { name, number } = request.body
+  Person.findByIdAndUpdate(request.params.id, { name, number}, {new:true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -81,7 +78,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -13,11 +13,32 @@ mongoose.connect(url)
     })
 
 const PersonSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true,
+        validate:{
+            validator:function(val){
+                const check1 = val.length>8;
+                const a = val.split("-")
+                if(a.length===1){
+                    return check1
+                } else if(a.length===2){
+                    const check2 = a[0].length===2 || a[0].length===3;
+                    const check3 = (a[0].match(/^[0-9]+$/) != null) && (a[1].match(/^[0-9]+$/) != null)
+                    return (check1 && check2 && check3)
+                } else{
+                    return false
+                }
+            },
+            message: 'Invalid number'
+        }
+    }
 })
-
-const Person = mongoose.model('Person', PersonSchema)
 
 PersonSchema.set('toJSON', {
     transform: (document, returnedObject) => {
